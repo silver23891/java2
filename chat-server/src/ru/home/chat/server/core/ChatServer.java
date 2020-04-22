9febd5eba351cadf1a404c54301d0e1b31dad79c
Anton <aborisov@yandex.ru>
2020-04-22 21:47:07 +0700

    module structure

---
+++ chat-server/src/ru/home/chat/server/core/ChatServer.java
@@ -0,0 +1,99 @@
+package ru.home.chat.server.core;
+
+import ru.home.network.ServerSocketThread;
+import ru.home.network.ServerSocketThreadListener;
+import ru.home.network.SocketThread;
+import ru.home.network.SocketThreadListener;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer implements ServerSocketThreadListener, SocketThreadListener {
+
+    ServerSocketThread server;
+
+    public void start(int port) {
+        if (server != null && server.isAlive())
+            putLog("Already running");
+        else
+            server = new ServerSocketThread(this, "Server", port, 2000);
+    }
+
+    public void stop() {
+        if (server == null || !server.isAlive()) {
+            putLog("Nothing to stop");
+        } else {
+            server.interrupt();
+        }
+    }
+
+    private void putLog(String msg) {
+        System.out.println(msg);
+    }
+
+    /**
+     * Server Socket Thread Listener methods
+     * */
+
+    @Override
+    public void onServerStarted(ServerSocketThread thread) {
+        putLog("Server thread started");
+    }
+
+    @Override
+    public void onServerCreated(ServerSocketThread thread, ServerSocket server) {
+        putLog("Server socket started");
+    }
+
+    @Override
+    public void onServerTimeout(ServerSocketThread thread, ServerSocket server) {
+        //putLog("Server timeout");
+    }
+
+    @Override
+    public void onSocketAccepted(ServerSocketThread thread, ServerSocket server, Socket socket) {
+        putLog("Client connected");
+        String name = "SocketThread " + socket.getInetAddress() + ":" + socket.getPort();
+        new SocketThread(this, name, socket);
+    }
+
+    @Override
+    public void onServerException(ServerSocketThread thread, Throwable throwable) {
+        putLog("Server exception");
+        throwable.printStackTrace();
+    }
+
+    @Override
+    public void onServerStop(ServerSocketThread thread) {
+        putLog("Server thread stopped");
+    }
+
+    /**
+     * Socket Thread Listener methods
+     * */
+
+    @Override
+    public void onSocketStart(SocketThread thread, Socket socket) {
+        putLog("Socket started");
+    }
+
+    @Override
+    public void onSocketStop(SocketThread thread) {
+        putLog("Socket stopped");
+    }
+
+    @Override
+    public void onSocketReady(SocketThread thread, Socket socket) {
+        putLog("Socket ready");
+    }
+
+    @Override
+    public void onReceiveString(SocketThread thread, Socket socket, String msg) {
+        thread.sendMessage("echo: " + msg);
+    }
+
+    @Override
+    public void onSocketException(SocketThread thread, Throwable throwable) {
+        throwable.printStackTrace();
+    }
+}
